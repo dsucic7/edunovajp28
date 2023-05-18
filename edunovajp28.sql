3b6e1a588c8f5f267a640831ef65ea997f36f7bb
Damir Sucic <131907383+dsucic7@users.noreply.github.com>
2023-05-18 19:50:38 +0200

    select i insert

--- edunovajp28.sql
+++ edunovajp28.sql
@@ -1,11 +1,13 @@
 drop database if exists edunovajp28;
 create database edunovajp28;
 
+-- # c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\dsuci\OneDrive\Documents\edunovajp28\edunovajp28.sql
+
 use edunovajp28;
 
 create table smjer(
 sifra int not null primary key auto_increment,
-naziv varchar(50) not null,  
+naziv varchar(50) not null,    
 cijena decimal(18,2),
 upisnina decimal (18,2),
 trajanje int not null,
@@ -61,4 +63,47 @@ alter table clan add foreign key (grupa) references grupa(sifra);
 
 
 
+-- najlosiji nacin unosa
 
+insert into smjer values
+(null,'Java programiranje ŠČ',995.42,73,130,true);
+
+-- malo bolji nacin unosa
+
+insert into smjer (naziv,trajanje) values
+('Web programiranje',250);
+  
+-- primjer dobre prakse (best practice)
+ 
+ insert into smjer (sifra,naziv,cijena,upisnina,trajanje,verificiran) values
+ (null,'PHP programiranje ', 850,73,130,false);
+
+select * from osoba; 
+
+insert into osoba (sifra,ime,prezime,oib,email) values
+
+(null,'Denis','Simov',null,'dsimov19@gmail.com'),
+(null,'Ivan','Islentiev',null,'ivanislentiev@hotmail.com'),
+(null,'Antonio','Majić',null,'antonio.majich@gmail.com'),
+(null,'Kristi','Belić',null,'kristinovoselic@gmail.com'),
+(null,'Josip','Belić',null,'josip.belic989@gmail.com'),
+(null,'Ivan','Marošević',null,'ivanmarosevic4@gmail.com'),
+(null,'Damir','Sučić',null,'dsucic7@gmail.com'),
+(null,'Dino','Dješka',null,'dino.djeska@gmail.com'),
+(null,'Goran','Kovačić',null,'kovacic.go@gmail.com'),
+(null,'Luka','Lijić',null,'lukalijic8@gmail.com'),
+(null,'Antonio','Miloloža',null,'milolozaantonio@yahoo.com'),
+(null,'Marijan','Kešinović',null,'kesinovic957@gmail.com'),
+(null,'Ivan','Kuna',null,'ivan.kuna@yahoo.com'),
+(null,'Maksimilian','Bagarić',null,'maxbagaric@gmail.com'),
+(null,'Martin','Tetković',null,'marcatet001@gmail.com'),
+(null,'Vjeran','Šergo',null,'vjeran.sergo@gmail.com'),
+(null,'Filip','Čatić',null,'fcatic031@gmail.com'),
+(null,'Petar','Matić',null,'petarmatic00@gmail.com'),
+(null,'Branimir','Fekete',null,'branimir.fekete@gmail.com'),
+(null,'Lazar','Volarev',null,'lazarv79@gmail.com'),
+(null,'Adriana','Plečaš',null,'adriana.plecas@gmail.com'),
+(null,'Goran','Franjić',null,'goran@proces.hr'),
+(null,'Marko','Ivanović',null,'markoivanovic19@gmail.com'),
+(null,'Emina','Vejsilović',null,'emiomiemi@gmail.com'),
+(null,'Ana','Jurić',null,'ana.juric19@gmail.com');
